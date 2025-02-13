ab76b0298825cb2ce1335af9c96a519eab55aa67
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-13 09:46:51 -0300

    Configuração inicial do projeto Spring Boot com: Spring Data JPA, MySQL e Spring Security

---
+++ src/main/java/com/proj/protime/ProtimeApplication.java
@@ -0,0 +1,13 @@
+package com.proj.protime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtimeApplication.class, args);
+	}
+
+}
