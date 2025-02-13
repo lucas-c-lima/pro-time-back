1bd4de8c6908d22bb13fb83107ceb903972b5d85
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-13 11:47:48 -0300

    Adiciona a entidade Projeto com atributos, validações e relacionamentos, além das enumerações PrioridadeProjeto e StatusProjeto

---
+++ src/main/java/com/proj/protime/entities/enums/PrioridadeProjeto.java
@@ -0,0 +1,7 @@
+package com.proj.protime.entities.enums;
+
+public enum PrioridadeProjeto {
+	ALTA,
+	MEDIA,
+	BAIXA
+}
---
+++ src/main/java/com/proj/protime/entities/enums/StatusProjeto.java
@@ -0,0 +1,8 @@
+package com.proj.protime.entities.enums;
+
+public enum StatusProjeto {
+	PLANEJADO,
+	EM_ANDAMENTO,
+	CONCLUIDO,
+	CANCELADO
+}
