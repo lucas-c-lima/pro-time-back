1093355f9be93a510bbb2fc53c63bd79ed0a7c1d
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-13 10:49:02 -0300

    Adiciona entidades Atividade, LancamentoHora, Projeto e Usuario com mapeamento JPA

---
+++ src/main/java/com/proj/protime/entities/LancamentoHora.java
@@ -0,0 +1,12 @@
+package com.proj.protime.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "lancamento_horas")
+public class LancamentoHora implements Serializable{
+
+}
---
+++ src/main/java/com/proj/protime/entities/Usuario.java
@@ -0,0 +1,12 @@
+package com.proj.protime.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable{
+	
+}
