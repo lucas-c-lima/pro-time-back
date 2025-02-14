8396c0930d8f4d2709ad9f325e93c5dd2ba8b8e9
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-14 10:08:25 -0300

    Adiciona controladores para as entidades Atividade, LancamentoHora, Projeto e Usuario

---
+++ src/main/java/controllers/LancamentoHoraController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lancamento")
+public class LancamentoHoraController {
+
+	// TODO main endpoints
+	
+}
---
+++ src/main/java/controllers/ProjetoController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/projeto")
+public class ProjetoController {
+
+	// TODO main endpoints
+	
+}
