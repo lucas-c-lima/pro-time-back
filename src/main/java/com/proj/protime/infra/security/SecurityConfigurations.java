9d5d50947d6810aa62b7ec207572ec2f8b3df493
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-22 17:44:52 -0300

    Adiciona configuração de autorização para acesso ao endpoint de adição de usuário, restringindo a operação a usuários com a função ADMIN

--- src/main/java/com/proj/protime/infra/security/SecurityConfigurations.java
+++ src/main/java/com/proj/protime/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.proj.protime.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,11 @@ public class SecurityConfigurations {
 		return httpSecurity
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				// Necessário token e ser admin para acessar add-user
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.POST, "/add-user").hasRole("ADMIN")
+						.anyRequest().authenticated()
+				)
 				.build();
 	}
 }
